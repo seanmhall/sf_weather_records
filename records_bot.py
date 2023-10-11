b4541dda7199bbd09fac206755b7d0bb8f2ba4d3
Sean M. Hall <73158558+seanmhall@users.noreply.github.com>
2023-10-10 23:50:05 -0700

    Update records_bot.py
    Simplified a few things and added some more comments.

--- records_bot.py
+++ records_bot.py
@@ -3,13 +3,14 @@ import requests, json
 from datetime import datetime
 
 DEBUG = False # Set this to True to print the results without posting to Mastodon
-MASTODON_API_TOKEN = "XXXXXXXXXXXXXXXXXXXXXX"
-DOWNTOWN_SF_WEATHER_STATION_ID = "047772"
+MASTODON_API_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+DOWNTOWN_SF_WEATHER_STATION_ID = "047772" #This value has to be a string since it has a leading zero
 
 # For missing values, we can't leave them as a string (namely "M") so convert them to a dummy numerical value
+# Must be a number that won't occur naturally in any temperature records
 MISSING_VALUE = 777
 
-#For adding a Leading Zero to a single-digit integer
+# For adding a Leading Zero to a single-digit integer
 LZ = lambda x : str(x) if x >= 10 else '0'+str(x)
 
 def ordinalize(x):
@@ -25,10 +26,6 @@ def ordinalize(x):
     else:
         ordinal = 'th'
     return str(x)+ordinal
-
-def convert_date_string(s): # Convert a YYYY-MM-DD string to a datetime object
-    x = [int(y) for y in s.split("-")]
-    return datetime(x[0], x[1], x[2])
 
 def record_year(y):
     if isinstance(y, int): # If y is a String (a single year)
@@ -46,32 +43,38 @@ def get_records(d, station_id=DOWNTOWN_SF_WEATHER_STATION_ID): # "d" is a dateti
                                {"name":"mint","interval":[1,0,0],"duration":"dly"},
                                {"name":"pcpn","interval":[1,0,0],"duration":"dly"}]
                       }
+    
     r = requests.post("http://data.rcc-acis.org/StnData", json=request_params, headers={'Accept':'application/json'})
     raw_data = json.loads(r.text)["data"]
     data = []
     
     for d in raw_data:
+
+        year = int(d[0][:4]) #The first four digits of the YYYY-MM-DD string
         high_temp = int(d[1]) if d[1] != "M" else MISSING_VALUE
         low_temp = int(d[2]) if d[2] != "M" else MISSING_VALUE
-
         pcpn = d[3]
+
         if pcpn == "T": # Convert T (for Trace amounts of rain) to a numerical value
             pcpn = 0.001
         elif pcpn == "M" or pcpn == "S": # Record for Febuary 6th 1982 has precipitation value "S" (??)
             pcpn = MISSING_VALUE
         else:
             pcpn = float(pcpn)
-        data.append([convert_date_string(d[0]), high_temp, low_temp, pcpn])
+
+        data.append([year, high_temp, low_temp, pcpn])
     
+    #Sort by high temperatures
     data.sort(key = lambda x: x[1])
 
     lowest_maxt = [x for x in data[0] if x != MISSING_VALUE]
     lowest_maxt_value = lowest_maxt[1]
-    lowest_maxt_year = [x[0].year for x in data if x[1] == lowest_maxt_value]
+    lowest_maxt_year = [x[0] for x in data if x[1] == lowest_maxt_value]
+    data = [x for x in data if MISSING_VALUE not in x]
 
     highest_maxt = [x for x in data[-1] if x != MISSING_VALUE]
     highest_maxt_value = highest_maxt[1]
-    highest_maxt_year = [x[0].year for x in reversed(data) if x[1] == highest_maxt_value]
+    highest_maxt_year = [x[0] for x in reversed(data) if x[1] == highest_maxt_value]
 
     output = {"max_temps": {
                     "lowest": {
@@ -85,15 +88,16 @@ def get_records(d, station_id=DOWNTOWN_SF_WEATHER_STATION_ID): # "d" is a dateti
                     }
     }
     
+    #Now sort by low temperatures
     data.sort(key = lambda x: x[2])
     
     lowest_mint = [x for x in data[0] if x != MISSING_VALUE]
     lowest_mint_value = lowest_mint[2]
-    lowest_mint_year = [x[0].year for x in data if int(x[2]) == lowest_mint_value]
+    lowest_mint_year = [x[0] for x in data if int(x[2]) == lowest_mint_value]
 
     highest_mint = [x for x in data[-1] if x != MISSING_VALUE]
     highest_mint_value = highest_mint[2]
-    highest_mint_year = [x[0].year for x in reversed(data) if int(x[2]) == highest_mint_value]
+    highest_mint_year = [x[0] for x in reversed(data) if int(x[2]) == highest_mint_value]
     
     output.update({"min_temps": {
                     "lowest": {
@@ -107,12 +111,12 @@ def get_records(d, station_id=DOWNTOWN_SF_WEATHER_STATION_ID): # "d" is a dateti
                     }
     })
 
-    #Sort by precipitation
+    #Finally, sort by precipitation
     data.sort(key = lambda x: x[3])
+    
     data = [x for x in data if x[3] != MISSING_VALUE]
-
     rainiest_day = data[-1]
-    output['precipitation'] = {"year": rainiest_day[0].year,
+    output['precipitation'] = {"year": rainiest_day[0],
                                 "amount": rainiest_day[3]}
     return output
 
